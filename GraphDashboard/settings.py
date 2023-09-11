f6a35030f1442f6e50a78942c7079140510fa18d
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-11 17:26:43 +0600

    configured PostgreSQL DB with Django

--- GraphDashboard/settings.py
+++ GraphDashboard/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'GraphDashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GraphDashboardDB',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
     }
 }
 
