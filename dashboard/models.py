01375db116e6d7c6806f2208e07f0ddd9c998709
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-12 09:23:54 +0600

    created and registered EmployeData model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeData(models.Model):
+    month = models.CharField(max_length=15)
+    attendace = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.month}-{self.attendace}'
