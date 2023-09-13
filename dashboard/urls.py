08041383844dea52811368cb028056e3eaf8a873
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-13 12:10:02 +0600

    created registration form and configured path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dashboard-index'),
+    path('', views.registration, name='registration'),
+    path('dashboard/', views.index, name='dashboard-index'),
 ]
