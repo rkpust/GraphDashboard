60a08ffe4aa7817ee5f2a1d8b3baefd53ffc2d2f
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-13 15:00:06 +0600

    inserted registration form data in DB and showed error message

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.registration, name='registration'),
+    path('', views.registration),
+    path('registration/', views.registration, name='registration'),
     path('dashboard/', views.index, name='dashboard-index'),
 ]
